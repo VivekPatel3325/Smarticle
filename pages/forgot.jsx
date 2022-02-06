31349a2374be3c25f8ad7a4a06acbc848f628c5f
Angela Gilhotra <an313550@dal.ca>
2022-02-06 15:25:46 -0400

    add button + style changes

--- pages/forgot.jsx
+++ pages/forgot.jsx
@@ -3,18 +3,21 @@ import Main from "layouts/main";
 const forgot = () => {
   return (
     <Main title="Forgot Password">
-      <div className="grid grid-cols-1 gap-2">
+      <div className="grid grid-cols-1 gap-2 w-96">
       <label htmlFor="email">
         Enter registered email:
       </label>
       <input
-        className="bg-slate-50 outline-none m-2 p-2 rounded-lg border-2 border-black"
+        className="bg-slate-50 outline-none p-2 rounded-lg border-2 border-black"
         type="email"
         name="email"
         id="email"
         placeholder="Email"
         required
       />
+      <button className="border-black border-2 rounded-md font-semibold hover:bg-black hover:text-white p-2 mt-4 w-1/2">
+          Submit
+        </button>
       </div>
     </Main>
   );
