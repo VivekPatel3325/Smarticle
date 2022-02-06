431983421f0a671fdcf33b8803037c4493b51e37
Angela Gilhotra <an313550@dal.ca>
2022-02-06 13:13:39 -0400

    add password matching

--- pages/Signup.js
+++ pages/Signup.js
@@ -1,19 +1,20 @@
 import Head from "next/head";
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
+import {useState, useEffect} from "react";
 
 const Signup = () => {
+  const [isMatching, setIsMatching] = useState(false);
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  useEffect(() => {
+    if (password === confirmPassword) setIsMatching(true);
+    else setIsMatching(false);
+  }, [password, confirmPassword])
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Signup</title>
-      </Head>
-      <main className={styles.main}>
+    <div>
+      <title>Signup</title>
+      <div>
         <div className="bg-gray-200 rounded-2xl shadow-2xl flex max-w-4xl">
           <div className="p-5">
-            <div>
-              <p className="text-1xl font-bold">Welcome :)</p>
-            </div>
             <div className="py-10">
               <p className="text-2xl font-bold mb-1">Signup</p>
             </div>
@@ -52,6 +53,7 @@ const Signup = () => {
                     minLength="8"
                     pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
                     required
+                    onChange={(e) => setPassword(e.target.value)}
                   />
                 </div>
               </div>
@@ -61,24 +63,26 @@ const Signup = () => {
                     className="bg-slate-50 outline-none flex-1 ml-5"
                     type="password"
                     name="password"
-                    id="password"
+                    id="confirm-password"
                     placeholder="Confirm Password"
                     minLength="8"
                     required
+                    onChange={(e) => setConfirmPassword(e.target.value)}
                   />
                 </div>
               </div>
             </div>
+
+            {!isMatching && <div>Password strings do not match. enter again.</div>}
+
             <div>
-              <Link href="/Signup">
-                <button className="border-black border-2 rounded-full px-7 py-2 font-semibold hover:bg-black hover:text-white">
-                  Signup
-                </button>
-              </Link>
+              <button className="border-black border-2 rounded-full px-7 py-2 font-semibold hover:bg-black hover:text-white">
+                Signup
+              </button>
             </div>
           </div>
         </div>
-      </main>
+      </div>
     </div>
   );
 };
