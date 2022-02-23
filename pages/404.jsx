750696a263e8f994383018fd931354f9bef6cf2b
Angela Gilhotra <an313550@dal.ca>
2022-02-23 00:32:15 -0400

    fix 404

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,16 +1,10 @@
 import Link from "next/link";
+import Main from "layouts/main";
 
 const NotFound = () => {
   return (
-    <div>
-      <div className="not-found">
-        <p className="font-bold text-5xl mb-2">404 Error</p>
-        <p className="font-semibold text-2xl mb-2">Page Not Found</p>
-        <Link href="/">
-          <a>Back to Home Page</a>
-        </Link>
-      </div>
-    </div>
+    <Main title="404.">
+    </Main>
   );
 };
 
