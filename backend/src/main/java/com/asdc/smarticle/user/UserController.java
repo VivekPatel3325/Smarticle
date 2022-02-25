9b087c0685b89e9ab97f5066fe004acf37f63d21
Vivekkumar Patel <vv662564@dal.ca>
2022-02-25 14:02:33 -0400

    added new fields to user entity

--- backend/src/main/java/com/asdc/smarticle/user/UserController.java
+++ backend/src/main/java/com/asdc/smarticle/user/UserController.java
@@ -2,12 +2,9 @@ package com.asdc.smarticle.user;
 
 import javax.mail.MessagingException;
 
-import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
-import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
