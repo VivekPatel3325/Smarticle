d035e169393e009a4d28b7e60b739018072dd79d
Angela Gilhotra <an313550@dal.ca>
2022-03-01 18:23:35 -0400

    home page

--- frontend/service/post.service.js
+++ frontend/service/post.service.js
@@ -1,11 +1,13 @@
 import { serverUrl } from "helpers/api";
-
+import posts from "data/posts.json";
 // post : {heading, content, visibility, tagid[]}
 const create = async (post) => {
   // @todo
   console.log(post);
 }
+const getAll = () => posts;
 
 export const postService = {
   create,
+  getAll
 }
