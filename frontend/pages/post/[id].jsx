4ac9972bc5416130cf334767b12bb927b801e07a
khushboo <kh923880@dal.ca>
2022-03-22 15:30:12 -0300

    Added UI for twitter details page

--- frontend/pages/post/[id].jsx
+++ frontend/pages/post/[id].jsx
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 import React, {useState, useEffect} from "react";
+=======
+import React, { useState, useEffect } from "react";
+>>>>>>> Stashed changes
 import Main from "layouts/main";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/router";
@@ -10,8 +14,15 @@ import { tagsService } from "service/tags.service";
 const PostDetails = () => {
   const router = useRouter();
   const user = useUser();
+<<<<<<< Updated upstream
   const {id} = router.query;
   const onClickLike = () => {console.log("Liked")}
+=======
+  const { id } = router.query;
+  const onClickLike = () => {
+    console.log("Liked");
+  };
+>>>>>>> Stashed changes
   const [post, setPost] = useState({
     heading: "",
     content: "",
@@ -20,6 +31,7 @@ const PostDetails = () => {
     author: {
       firstname: "",
       lastname: "",
+<<<<<<< Updated upstream
       username: ""
     },
     tags: []
@@ -28,6 +40,16 @@ const PostDetails = () => {
     const fetch = async () => {
       const post = await postService.getById(user?.token, id)
       const tags = await tagsService.getByIds(user?.token, post.tagId)
+=======
+      username: "",
+    },
+    tags: [],
+  });
+  useEffect(() => {
+    const fetch = async () => {
+      const post = await postService.getById(user?.token, id);
+      const tags = await tagsService.getByIds(user?.token, post.tagId);
+>>>>>>> Stashed changes
       setPost({
         heading: post.heading,
         content: post.content,
@@ -36,17 +58,26 @@ const PostDetails = () => {
         author: {
           firstname: post.userId?.firstName,
           lastname: post.userId?.lastName,
+<<<<<<< Updated upstream
           username: post.userId?.userName
         },
         tags
       })
     }
+=======
+          username: post.userId?.userName,
+        },
+        tags,
+      });
+    };
+>>>>>>> Stashed changes
     if (id) {
       fetch();
     }
   }, [id]);
   return (
     <Main>
+<<<<<<< Updated upstream
           <div className="">
             <div className="flex flex-row justify-between">
               <div>
@@ -81,9 +112,167 @@ const PostDetails = () => {
             }
             </div>
             <FontAwesomeIcon className="my-3 text-3xl opacity-50 hover:opacity-100 hover:cursor-pointer" icon="thumbs-up" onClick={onClickLike} />
+=======
+      <div class="grid lg:grid-cols-2 lg:gap-48">
+        <div className="">
+          <div className="flex flex-row justify-between mb-10">
+            <div className="lg:mr-10">
+              <FontAwesomeIcon className="ml-1" icon="user" />
+              <p className="inline text-gray-700 ml-2 font-medium text-lg">
+                {post.author.firstname}&nbsp;{post.author.lastname}
+              </p>
+            </div>
+            <div>
+              <FontAwesomeIcon className="ml-1" icon="calendar" />
+              <p className="inline text-gray-700 ml-2 font-medium text-lg">
+                {moment(post.createdOn).format("MMM DD, YYYY")}
+              </p>
+            </div>
+          </div>
+          <div className="text-4xl my-4 text-center mb-10">{post.heading}</div>
+          <article
+            className="text-left mb-10"
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          />
+          <div className="mt-4">
+            {post.tags.map((tag) => {
+              return (
+                <span className="px-1.5 bg-white border-black border-2 rounded-lg mr-2">
+                  {tag.label}
+                </span>
+              );
+            })}
+          </div>
+          <FontAwesomeIcon
+            className="my-3 text-2xl opacity-50 hover:opacity-100 hover:cursor-pointer"
+            icon="thumbs-up"
+            onClick={onClickLike}
+          />
+        </div>
+        <div className="">
+          <div className="bg-gray-100 flex flex-col justify-center mb-5">
+            <div className="relative py-1 sm:max-w-sm sm:mx-auto">
+              <div className="rounded border border-gray-300 px-6 py-1 my-1">
+                <div className="flex items-center">
+                  <a
+                    className="flex h-8 w-8"
+                    href="https://twitter.com/naval"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      src="https://pbs.twimg.com/profile_images/1256841238298292232/ycqwaMI2_400x400.jpg"
+                      className="rounded-full"
+                    />
+                  </a>
+                  <a
+                    href="https://twitter.com/naval"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex flex-col ml-2"
+                  >
+                    <span
+                      className="flex items-center font-bold text-gray-900 leading-5"
+                      title="{author.name}"
+                    >
+                      Naval
+                      <svg
+                        aria-label="Verified Account"
+                        className="ml-1 text-blue-500 inline h-4 w-4"
+                        viewBox="0 0 24 24"
+                      >
+                        <g fill="currentColor">
+                          <path d="M22.5 12.5c0-1.58-.875-2.95-2.148-3.6.154-.435.238-.905.238-1.4 0-2.21-1.71-3.998-3.818-3.998-.47 0-.92.084-1.336.25C14.818 2.415 13.51 1.5 12 1.5s-2.816.917-3.437 2.25c-.415-.165-.866-.25-1.336-.25-2.11 0-3.818 1.79-3.818 4 0 .494.083.964.237 1.4-1.272.65-2.147 2.018-2.147 3.6 0 1.495.782 2.798 1.942 3.486-.02.17-.032.34-.032.514 0 2.21 1.708 4 3.818 4 .47 0 .92-.086 1.335-.25.62 1.334 1.926 2.25 3.437 2.25 1.512 0 2.818-.916 3.437-2.25.415.163.865.248 1.336.248 2.11 0 3.818-1.79 3.818-4 0-.174-.012-.344-.033-.513 1.158-.687 1.943-1.99 1.943-3.484zm-6.616-3.334l-4.334 6.5c-.145.217-.382.334-.625.334-.143 0-.288-.04-.416-.126l-.115-.094-2.415-2.415c-.293-.293-.293-.768 0-1.06s.768-.294 1.06 0l1.77 1.767 3.825-5.74c.23-.345.696-.436 1.04-.207.346.23.44.696.21 1.04z" />
+                        </g>
+                      </svg>
+                    </span>
+                  </a>
+                  <FontAwesomeIcon
+                    icon="fa-brands fa-twitter"
+                    className="ml-3 lg:ml-auto text-blue-400 h-4 w-4"
+                  />
+                </div>
+                <div className="mt-3 mb-3 leading-snug whitespace-pre-wrap text-sm text-gray-700">
+                  Learn to sell. Learn to build. If you can do both, you will be
+                  unstoppable.
+                </div>
+                <a
+                  className="text-gray-500 text-sm hover:underline"
+                  href="https://twitter.com/naval/status/1002104154737684480"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {" "}
+                  <p>Date</p>{" "}
+                </a>
+                <div className="flex text-gray-700 mt-3">
+                  <a
+                    className="flex items-center mr-3 text-gray-500 hover:text-blue-600 transition hover:underline"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <svg
+                      className="mr-1"
+                      width="14"
+                      height="14"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        className="fill-current"
+                        d="M14.046 2.242l-4.148-.01h-.002c-4.374 0-7.8 3.427-7.8 7.802 0 4.098 3.186 7.206 7.465 7.37v3.828c0 .108.045.286.12.403.143.225.385.347.633.347.138 0 .277-.038.402-.118.264-.168 6.473-4.14 8.088-5.506 1.902-1.61 3.04-3.97 3.043-6.312v-.017c-.006-4.368-3.43-7.788-7.8-7.79zm3.787 12.972c-1.134.96-4.862 3.405-6.772 4.643V16.67c0-.414-.334-.75-.75-.75h-.395c-3.66 0-6.318-2.476-6.318-5.886 0-3.534 2.768-6.302 6.3-6.302l4.147.01h.002c3.532 0 6.3 2.766 6.302 6.296-.003 1.91-.942 3.844-2.514 5.176z"
+                      />
+                    </svg>
+                    <span className="text-sm">117</span>
+                  </a>
+                  <a
+                    className="flex items-center mr-3 text-gray-500 hover:text-green-600 transition hover:underline"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <svg
+                      className="mr-1"
+                      width="14"
+                      height="14"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        className="fill-current"
+                        d="M23.77 15.67c-.292-.293-.767-.293-1.06 0l-2.22 2.22V7.65c0-2.068-1.683-3.75-3.75-3.75h-5.85c-.414 0-.75.336-.75.75s.336.75.75.75h5.85c1.24 0 2.25 1.01 2.25 2.25v10.24l-2.22-2.22c-.293-.293-.768-.293-1.06 0s-.294.768 0 1.06l3.5 3.5c.145.147.337.22.53.22s.383-.072.53-.22l3.5-3.5c.294-.292.294-.767 0-1.06zm-10.66 3.28H7.26c-1.24 0-2.25-1.01-2.25-2.25V6.46l2.22 2.22c.148.147.34.22.532.22s.384-.073.53-.22c.293-.293.293-.768 0-1.06l-3.5-3.5c-.293-.294-.768-.294-1.06 0l-3.5 3.5c-.294.292-.294.767 0 1.06s.767.293 1.06 0l2.22-2.22V16.7c0 2.068 1.683 3.75 3.75 3.75h5.85c.414 0 .75-.336.75-.75s-.337-.75-.75-.75z"
+                      />
+                    </svg>
+                    <span className="text-sm">5,434</span>
+                  </a>
+                  <a
+                    className="flex items-center text-gray-500 hover:text-red-600 transition hover:underline"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <svg
+                      className="mr-1"
+                      width="14"
+                      height="14"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        className="fill-current"
+                        d="M12 21.638h-.014C9.403 21.59 1.95 14.856 1.95 8.478c0-3.064 2.525-5.754 5.403-5.754 2.29 0 3.83 1.58 4.646 2.73.813-1.148 2.353-2.73 4.644-2.73 2.88 0 5.404 2.69 5.404 5.755 0 6.375-7.454 13.11-10.037 13.156H12zM7.354 4.225c-2.08 0-3.903 1.988-3.903 4.255 0 5.74 7.035 11.596 8.55 11.658 1.52-.062 8.55-5.917 8.55-11.658 0-2.267-1.822-4.255-3.902-4.255-2.528 0-3.94 2.936-3.952 2.965-.23.562-1.156.562-1.387 0-.015-.03-1.426-2.965-3.955-2.965z"
+                      />
+                    </svg>
+                    <span className="text-sm">23,519</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+>>>>>>> Stashed changes
       </div>
     </Main>
   );
 };
 
+<<<<<<< Updated upstream
 export default PostDetails;
+=======
+export default PostDetails;
+>>>>>>> Stashed changes
