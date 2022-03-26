a16758994315bc288eb3528991a020d44e86321a
Vivekkumar Patel <vv662564@dal.ca>
2022-03-26 06:20:10 -0300

    changed request type from http to https

--- frontend/helpers/api.js
+++ frontend/helpers/api.js
@@ -2,5 +2,5 @@ export const apiUrl =  process.env.NODE_ENV === "development"
   ? "http://localhost:3000/api" // development api
   : "http://localhost:3000/api"; // production api
 export const serverUrl = process.env.NODE_ENV === "development"
-? "http://smarticleasdc.herokuapp.com/smarticleapi" // development api
-: "http://smarticleasdc.herokuapp.com/smarticleapi"; // production api
+? "https://smarticleasdc.herokuapp.com/smarticleapi" // development api
+: "https://smarticleasdc.herokuapp.com/smarticleapi"; // production api
